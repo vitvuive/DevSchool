780b9a98e57ba8ec6c5ba0efe3af75f54d32aa33
Viet Nguyen <vietqb9779@gmail.com>
2019-09-16 00:17:00 +0700

    Setting RNN for android

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 /**
  * @format
  */
+import { Navigation } from "react-native-navigation";
+import App from "./App";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent("App", () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "App"
+      }
+    }
+  });
+});
