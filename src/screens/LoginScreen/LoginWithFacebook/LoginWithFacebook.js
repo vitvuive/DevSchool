25543ae26a608cf3a98fabb81a65cbfbe0c76ac0
Viet Nguyen <vietqb9779@gmail.com>
2019-10-13 16:17:00 +0700

    Add Login FB button

---
+++ src/screens/LoginScreen/LoginWithFacebook/LoginWithFacebook.js
@@ -0,0 +1,11 @@
+import React, { Component, } from 'react';
+import { View, } from 'react-native';
+
+class LoginWithFacebook extends Component {
+  state = {};
+  render() {
+    return <View></View>;
+  }
+}
+
+export default LoginWithFacebook;
