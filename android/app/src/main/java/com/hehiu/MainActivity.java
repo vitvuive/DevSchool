22aec5cb80ae716d653a9168e20cad8eb925f2e1
Viet Nguyen <vietqb9779@gmail.com>
2019-11-21 22:55:42 +0700

    Add flash screen

--- android/app/src/main/java/com/hehiu/MainActivity.java
+++ android/app/src/main/java/com/hehiu/MainActivity.java
@@ -1,9 +1,15 @@
 package com.hehiu;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 
-import com.facebook.react.ReactActivity;
 import com.reactnativenavigation.NavigationActivity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
 
 public class MainActivity extends NavigationActivity {
 
@@ -14,11 +20,22 @@ public class MainActivity extends NavigationActivity {
     protected String getMainComponentName() {
         return "hehiu";
     }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(this.createSplashLayout());
+    }
     
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent intent){
         super.onActivityResult(requestCode,resultCode,intent);
         MainApplication.getCallbackManager().onActivityResult(requestCode,resultCode,intent);
+    }
+
+    @SuppressLint("InflateParams")
+    public LinearLayout createSplashLayout(){
+        return (LinearLayout) LayoutInflater.from(this).inflate(R.layout.splash_layout, null);
     }
 }
