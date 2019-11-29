0251e906ff0cddaa9cf0eb6969b821207503de64
Viet Nguyen <vietqb9779@gmail.com>
2019-11-29 17:39:48 +0700

    Update put in saga checkout

--- src/sagas/cartSagas/checkOut.js
+++ src/sagas/cartSagas/checkOut.js
@@ -6,6 +6,8 @@ export default function* checkOut() {
   try {
     yield put(actions.cart.setCartLoadingStatus(true));
     yield call(API.CartApi.checkOut);
+    const data = yield call(API.CartApi.getTransaction);
+    yield put(actions.cart.getTransactionData(data));
   } catch (error) {
     // eslint-disable-next-line no-console
     console.log(error);
