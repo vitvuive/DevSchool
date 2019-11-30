c9753edddf30dd61129606d5eac7e593be5ba5ac
tuandoan <doan.manhtuan511@gmail.com>
2019-11-30 17:00:24 +0700

    modify loginScreen

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -26,7 +26,7 @@ export default class LoginScreen extends Component {
     login(username, password);
   };
 
-  _createUser = () => {
+  _Register = () => {
     const { username, password } = this.state;
     const register = this.props.register;
     register(username, password);
@@ -44,7 +44,6 @@ export default class LoginScreen extends Component {
             style={styles.login_box}
             placeholder={"email"}
             placeholderTextColor={"white"}
-            inlineImageLeft={"username"}
             keyboardType={"email-address"}
             onChangeText={username => this.setState({ username: username })}
           />
@@ -52,26 +51,35 @@ export default class LoginScreen extends Component {
             style={styles.login_box}
             placeholder={"password"}
             placeholderTextColor={"white"}
-            inlineImageLeft={"password"}
             secureTextEntry={true}
             onChangeText={password => this.setState({ password: password })}
           />
-          <Text></Text>
-          <TouchableOpacity style={styles.login_btn} onPress={this._Login}>
-            <Text style={styles.login_label}>Login</Text>
+          <TouchableOpacity style={styles.forgot_password_btn}>
+            <Text style={{ fontSize: 15, color: "white" }}>
+              Forgot password?
+            </Text>
           </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.create_user_btn}
-            onPress={this._createUser}
-          >
-            <Text style={styles.create_label}>Create an account</Text>
-          </TouchableOpacity>
+          <StyledButton
+            onPress={this._Login}
+            title={"Login"}
+            textColor={"white"}
+            backgroundColor={Colors.accent}
+            containerStyle={styles.login_btn}
+          />
+          <Text style={{ color: "white", fontSize: 15 }}>- or -</Text>
+          <StyledButton
+            onPress={onPress}
+            title={"Login with Facebook"}
+            textColor={"white"}
+            backgroundColor={Colors.accent}
+            containerStyle={styles.login_facebook_btn}
+          />
         </View>
         <StyledButton
-          onPress={onPress}
-          title={"Login with Facebook"}
+          onPress={this._Register}
+          title={"Create an account"}
           textColor={"white"}
-          backgroundColor={Colors.accent}
+          containerStyle={styles.register_btn}
         />
       </View>
     );
@@ -91,16 +99,30 @@ const styles = StyleSheet.create({
   login_area: {
     alignItems: "center"
   },
-  login_label: {
-    fontSize: 30,
-    color: "white"
+  login_btn: {
+    width: 250,
+    marginTop: 10
+  },
+  login_facebook_btn: {
+    width: 250,
+    marginTop: 5
   },
   login_box: {
-    width: "65%",
+    width: "70%",
     fontSize: 20,
     borderBottomColor: "white",
     borderBottomWidth: 1,
     color: "white"
   },
-  login_btn: {}
+  register_btn: {
+    shadowOffset: { width: 0, height: 0 },
+    elevation: 0,
+    shadowRadius: 15,
+    shadowOpacity: 0,
+    shadowColor: "rgba(0, 0, 0, 0)"
+  },
+  forgot_password_btn: {
+    marginLeft: "35%",
+    marginTop: 10
+  }
 });
