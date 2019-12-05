001e78c3fe9c9067ad8d5a1f6751f9dfba3dca4e
Viet Nguyen <vietqb9779@gmail.com>
2019-12-05 13:32:34 +0700

    Config connect server with devce

--- src/values/ConfigApi.js
+++ src/values/ConfigApi.js
@@ -1,8 +1,11 @@
 const token =
   'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTc1MzQyNTU3LCJqdGkiOiIzMzM2YTZiOWVhNjE0NzZhOWM3NTkyZGJjMDgzOWI0MyIsInVzZXJfaWQiOjF9.vOalCmLE8UWYckoGFoZFNkByAriJ7mBzq-xfX3bbv1A';
 //for emulator
-const portIP = '10.0.2.2';
+// const portIP = '10.0.2.2';
+const portIP = '127.0.0.1';
 
 //for device, get from IPv4
-// const portIP = "10.0.2.2";
+// const portIP = '172.16.19.48';
+// const portIP = '172.16.10.199';
+// const portIP = '0.0.0.0';
 export default { token, portIP, };
